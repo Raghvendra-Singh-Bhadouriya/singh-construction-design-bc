60eb49f6f109f8ee7bd1624ecc4f2759b8f6d9ff
Raghvendra-Singh-Bhadouriya <raghvendrabhadouriya2003@gmail.com>
2025-08-16 20:09:22 +0530

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const server = express();
 const connection = require("./config/db")
 
